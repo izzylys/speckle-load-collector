18a91233c3c8d19c259a0a13385d0f0ebd3fb774
izzy lyseggen <izzy.lyseggen@gmail.com>
2019-02-14 10:20:51 +0000

    example profile

---
+++ sampleProfile.py
@@ -0,0 +1,18 @@
+import simplejson as json
+# profile for xxxx
+creds = {
+    'role': 'mechanical engineer',
+    'apitoken': '{insert your key here}',
+    'name': 'izzy',
+    'surname': 'lyseggen',
+    'company':'Arup',
+    'email': 'izzy.lyseggen@gmail.com',
+    'server': 'https://hestia.speckle.works/api/v1',
+    'server_name': 'hestia',
+}
+
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': '{insert your key here}',
+}
